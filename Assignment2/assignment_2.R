dab6940e1c4d7cfeef411a54bf385e662ffccd79
Aless1234 <s212246@dtu.dk>
2025-03-17 20:25:09 +0100

    add folder and file for assignment 2

---
+++ Assignment2/assignment_2.R
@@ -0,0 +1,9 @@
+# Libraries to be used in this assignment
+
+setwd("Assignment2")
+print(getwd())
+
+# ----------------------------- Exercise 1 -----------------------------------
+
+
+# ----------------------------- Exercise 2 -----------------------------------
