2abea499274110ea7e3847eab5a68e0929dbf975
Alessia Saccardo <s212246@dtu.dk>
2025-04-03 15:25:16 +0200

    create R file

---
+++ Assignment3/Assignment_3.R
@@ -0,0 +1,19 @@
+# Libraries to be used in this assignment
+# install.packages("forecast")
+library(ggplot2)
+library(forecast)
+
+setwd("Assignment3")
+print(getwd())
+
+# ----------------------------- Exercise 1 -----------------------------------
+
+
+
+
+# ----------------------------- Exercise 2 -----------------------------------
+
+
+
+
+# ----------------------------- Exercise 3 -----------------------------------
