e85e095d9cf88399a8699b9fd20eee4c2dec2a71
Alessia Saccardo <s212246@dtu.dk>
2025-05-05 18:15:31 +0200

    add folder and data for assignment 4

---
+++ Assignment4/Assignment_4.R
@@ -0,0 +1,14 @@
+library(ggplot2)
+library(forecast)
+library(tidyr)
+library(ggpubr)
+
+setwd("Assignment3")
+print(getwd())
+
+# ------------------------------- Exercise 1 -------------------------------
+
+
+
+
+# ------------------------------- Exercise 2 -------------------------------
